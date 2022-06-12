08dd3b5fd0492d21aafa2617fe54e7c6e1767bc0
shuhei-T <81573592+shuhei-T@users.noreply.github.com>
2022-06-12 15:12:41 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export const App = () => {
+  console.log("App");
+
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+    </div>
+  );
+};
